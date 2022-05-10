6e28f71cf3e951a3a6303118b4d0ba79c672975e
Шахриёр <84191677+Shahriyor-dev@users.noreply.github.com>
2022-05-10 19:25:14 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -161,12 +161,22 @@ const swiper = new Swiper(".swiper-main", {
         renderBullet(index, className) {
             const thumb = this.slides[index].dataset.thumb;
             const text = this.slides[index].dataset.text;
-            return (
-                `<div class="${className}">
-                    <img class="swiper-pagination-image" src="${thumb}">
-                    <span>${text}</span>
-                </div>`
-            );
+            return `<div class="${className}">
+                <div class="swiper-pagination-image-wrapper">
+                  <img class="swiper-pagination-image" src="${thumb}">
+                  </div>
+                <span>${text}</span>
+                <div class="swiper-pagination-bg swiper-pagination-bg-left-side">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div class="swiper-pagination-bg swiper-pagination-bg-right-side">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                </div>`;
         }
     },
 });
